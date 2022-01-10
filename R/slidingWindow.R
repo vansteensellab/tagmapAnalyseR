2fa6a30046c0f271a5128899dda904357efc6f1d
k.rademaker <k.rademaker@nki.nl>
2022-01-10 12:59:32 +0100

    added function to calculate fraction of insertions across experimental conditions in sliding windows

--- R/slidingWindow.R
+++ R/slidingWindow.R
@@ -16,20 +16,32 @@
 #' @param windowSize Size of each window, e.g. 1e3 or 1e4 bp long.
 #' @param stepSize Size that each step increases, from minimal steps of 1 bp to
 #' any other custom value.
+#' @param mode Readout mode, eiter 'counts' for number of insertions per sliding
+#' window or 'fraction' for number of insertions per sliding across experimental
+#' conditions.
+#' @param numerator Experimental condition(s) for numerator of fraction of
+#' insertions (only required for fraction readout).
+#' @param denominator Experimental condition(s) for denominator of fraction of
+#' insertions (only required for fraction readout).
 #'
 #' @return Returns data table for putative insertion sites outputted by the
 #' TagMap pipeline.
 #' \describe{
 #' \item{windowStart, windowEnd}{Chromosome start/end positions of a specific
 #' window}
-#' \item{readout}{Readout of the window, such as number of integrations}
+#' \item{readout}{Readout of the sliding window, e.g. number of integrations}
 #' }
 #'
 #' @importFrom tibble tibble
 #' @importFrom dplyr %>%
 #' @importFrom utils tail
 #' @export
-chromosomeSlidingWindow <- function(data, chromosome, windowStart, windowEnd, windowSize, stepSize) {
+chromosomeSlidingWindow <- function(data, chromosome, windowStart, windowEnd, windowSize = 10000, stepSize = 1000, mode, numerator = NULL, denominator = NULL) {
+  # Test readout mode formatting
+  if (!tolower(mode) %in% c('counts', 'fraction')){
+    stop("Readout mode incorrect, use either 'counts' or 'fraction'.")
+  }
+
   # Calculate sliding window start positions.
   windows <- seq(
     from = windowStart,
@@ -47,12 +59,26 @@ chromosomeSlidingWindow <- function(data, chromosome, windowStart, windowEnd, wi
   windowReadout <- vector(length = length(windows))
   # Iterate windows to get readout specified by custom function.
   for (i in 1:length(windows)) {
-    windowReadout[i] <- slidingWindowReadoutCounts(
-      data = data,
-      chromosome = chromosome,
-      windowStart = windows[i],
-      windowEnd = (windows[i] + windowSize)
-    )
+    # Readout: Number of insertions
+    if(tolower(mode) == 'counts'){
+      windowReadout[i] <- slidingWindowReadoutCounts(
+        data = data,
+        chromosome = chromosome,
+        windowStart = windows[i],
+        windowEnd = (windows[i] + windowSize)
+      )
+    }
+    # Readout: Fraction of insertions between experimental conditions
+    if(tolower(mode) == 'fraction'){
+      windowReadout[i] <- slidingWindowReadoutCounts(
+        data = data,
+        chromosome = chromosome,
+        windowStart = windows[i],
+        windowEnd = (windows[i] + windowSize),
+        numerator = numerator,
+        denominator = denominator
+      )
+    }
   }
 
   return(tibble::tibble(
@@ -70,8 +96,10 @@ chromosomeSlidingWindow <- function(data, chromosome, windowStart, windowEnd, wi
 #' Subsets the complete data object to only contain insertion site data within
 #' the genomic coordinates of the sliding window.
 #'
+#'
 #' @author Koen Rademaker, \email{k.rademaker@nki.nl}
-#' @param data Insertion site data (as formatted by TagMap pipeline).
+#' @param data Insertion site data (as formatted by TagMap pipeline), columns
+#' should minimally include- and be named 'chr', 'start' and 'end.
 #' @param chromosome Chromosome, formatted as "chr(number)".
 #' @param windowStart Window start position.
 #' @param windowEnd Window end position.
@@ -82,4 +110,47 @@ chromosomeSlidingWindow <- function(data, chromosome, windowStart, windowEnd, wi
 #' @export
 slidingWindowReadoutCounts <- function(data, chromosome, windowStart, windowEnd) {
   return(nrow(data %>% dplyr::filter(chr == chromosome & start >= windowStart & end <= windowEnd)))
+}
+
+
+
+#' Calculate the fraction of insertions between experimental conditions
+#' within a sliding window
+#'
+#' Subsets the complete data object to only contain insertion site data within
+#' the genomic coordinates of the sliding window. Next, insertions are divided
+#' by experimental conditions ('class' column) into the numerator insertions and
+#' denominator insertions (e.g. effect vs. control, respectively). The fraction
+#' of insertions is calculated by dividing the numerator by the denominator, a
+#' pseudocount of 1 is added for zero-values of the denominator.
+#'
+#' @author Koen Rademaker, \email{k.rademaker@nki.nl}
+#' @param data Insertion site data (as formatted by TagMap pipeline), columns
+#' should minimally include and be formatted as 'chr', 'start', 'end' and
+#' 'class'.
+#' @param chromosome Chromosome, formatted as "chr(number)".
+#' @param windowStart Window start position.
+#' @param windowEnd Window end position.
+#' @param numerator Names of experimental condition(s) in column 'class' of data
+#' to select numerator insertions for the fraction.
+#' @param denominator of experimental condition(s) in column 'class' of
+#' data to select denominator insertions for the fraction.
+#'
+#' @return Returns the fraction of insertions between experimental conditions.
+#'
+#' @importFrom dplyr filter %>%
+#' @export
+slidingWindowReadoutFraction <- function(data, chromosome, windowStart, windowEnd, numerator, denominator, pseudocount = 1) {
+  # Select subsets of numerator and denominator insertions
+  insertions <- data %>% dplyr::filter(chr == chromosome & start >= windowStart & end <= windowEnd)
+  insertionsNumerator <- nrow(insertions %>% dplyr::filter(class %in% numerator))
+  insertionsDenominator <- nrow(insertions %>% dplyr::filter(class %in% denominator))
+
+  # Add pseudocount of 1 to zero-values of the denominator
+  if(insertionsDenominator == 0){
+    insertionsDenominator <- insertionsDenominator + pseudocount
+  }
+
+  # Return fraction
+  return(insertionsNumerator / insertionsDenominator)
 }
